83120198dcd3abef7b96ff4142e7595860e86194
thawdiisen <thawdiisen@gmail.com>
2013-07-18 07:23:04 +0000

    hw4-part1

--- web.js
+++ web.js
@@ -1,18 +1,15 @@
-var fs = require('fs');
-var infile = "index.html";
-var txt = fs.readFileSync(infile);
-var buf = new Buffer(txt);
-var txtreplace=buf.toString('utf-8');
-
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(txtreplace);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
