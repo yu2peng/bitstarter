279b8a73bb4860df7cad878e5485be92198105ef
thawdiisen <thawdiisen@gmail.com>
2013-07-18 07:17:35 +0000

    finish hw4-part3

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+//var URL_DEFAULT = "http://shrouded-sea-7174.herokuapp.com/";
+var outfile = "getIndex.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,39 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(outfile, checkfile){
+    var response2main = function(result, response){
+        if (result instanceof Error) {
+            console.error('Error:' + util.format(response.message));
+        } else {
+            fs.writeFileSync(outfile, result);
+            var checkJson = checkHtmlFile(outfile, checkfile);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
+    };
+    return response2main;
+}
+
+var callCheckJson = function(inurl, outfile, checkfile){
+    var response2main = buildfn(outfile, checkfile);
+    rest.get(inurl).on('complete', response2main);
+};
+
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url')
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .parse(process.argv); 
+    if (program.url){ 
+        callCheckJson(program.url, outfile, program.checks);
+    }
+    else{
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
